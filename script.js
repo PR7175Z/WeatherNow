c1c11634d01f70471ccbe8bd76bd124fdda984bd
PR7175Z <aadimpraz@gmail.com>
2024-07-06 11:53:14 +0545

    Successfully connected weather api and ipapi

--- script.js
+++ script.js
@@ -1,38 +1,30 @@
 // const { stringify } = require("querystring");
-
-const cityname = "Bhaktapur";
 const key = "a50354433aba4e55b10235551240407";
-const api = `https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${cityname}`;
+
+async function geolocation(){
+    const res = await fetch('https://ipapi.co/json');
+    val = await res.json();
+    return val;
+}
 
 
-async function apicall(){
+async function apicall(apikey){
     let val = {};
-    const res = await fetch(api);
+    const res = await fetch(apikey);
     val = await res.json();
     return val;
 }
 
 document.addEventListener('DOMContentLoaded', async ()=>{
-    let apival = await apicall();
-    document.getElementById('viewport').innerHTML = JSON.stringify(apival, null, 2);
+    let val = await geolocation();
+    // let cityname = val.city;
+    let lat = val.latitude;
+    let long = val.longitude;
+    // console.log(val);
+    const apikey = `https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${lat}, ${long}`;
+    console.log(val.latitude);
+    console.log(val.longitude);
+    let apival = await apicall(apikey);
+    console.log(JSON.stringify(apival, null, 2));
+    document.getElementById('viewport').innerHTML = apival.current.temp_c;
 })
-
-// const fields = "https://api.sportmonks.com/v3/core/countries/api_token=FcRWYYcAHFdGkronfEwUetN1r1P5biuvoyPDEYbBx2E2523EWeqyJDo7iN1Q";
-// async function country_apicall(){
-//     let val = {};
-//     const res = await fetch(fields);
-//     val = await res.json();
-//     console.log(val);
-// }
-// country_apicall();
-
-
-// fetch(api)
-// .then(response=>response.json())
-// .then(data => {
-//     val = data;
-// })
-// .catch(error => console.error('Error', error))
-
-
-// console.log(JSON.stringify(val, null, 2))
